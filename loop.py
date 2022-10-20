f9da5b320e08a95cb5ca603db36dc842b3ed8658
Alex Smith <alexander.h.smith@avanade.com>
2022-10-20 14:42:29 +0100

    upload iterations exercises

---
+++ loop.py
@@ -0,0 +1,12 @@
+#File 1 > prints number 1-100
+for i in range(101):
+    print(i)
+
+#File 2 > prints numbers from 5 to 9
+for i in range(5,10):
+    print(i)
+
+#File 3 > prints all evens between 10 and 20 inclusive
+for i in range(10,21,2):
+    print(i)
+    
---
+++ zero.py
@@ -0,0 +1,9 @@
+# count = 0
+# while count <= 3:
+#     print(count)
+#####will create an infinite loop if run
+
+count = 0
+while count <=3:
+    print(count)
+    count = count+1
