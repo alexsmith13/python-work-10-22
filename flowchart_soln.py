c43388d86de49a521bacac995785ef077ea68d61
Alex Smith <alexander.h.smith@avanade.com>
2022-10-20 11:57:22 +0100

    upgrade flowchart soln

--- flowchart_soln.py
+++ flowchart_soln.py
@@ -3,14 +3,20 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-if a>b:
-    if a>c:
-        print(a)
-    else:
-        print(c)
-else:    
-    if b>c:
-        print(b)
-    else:
-        print(c)
+# if a>b:
+#     if a>c:
+#         print(a)
+#     else:
+#         print(c)
+# else:    
+#     if b>c:
+#         print(b)
+#     else:
+#         print(c)
         
+array = []
+array.append(a)
+array.append(b)
+array.append(c)
+print(max(array))
+ #^^ much more efficient method
